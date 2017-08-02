bbc136ad3783ce2f4397a82594c556a9bc4ac198
Henk Bakker <hbakker@quintor.local>
2017-08-02 15:23:15 +0200

    add react demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    let dataURL = "http://localhost/wp-json/wp/v2/movies?_embed";
+    fetch(dataURL)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          movies: res
+        })
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    let movies = this.state.movies.map((movie, index) => {
+      return <div key={index}>
+        <img src={movie._embedded['wp:featuredmedia'][0].media_details.sizes.large.source_url} />
+        <p><strong>Title:</strong> {movie.title.rendered}</p>
+        <p><strong>Release Year:</strong> {movie.acf.release_year}</p>
+        <p><strong>Rating:</strong> {movie.acf.rating}</p>
+        <div>
+          <strong>Description:</strong>
+          <div dangerouslySetInnerHTML={ {__html: movie.acf.description} } />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    });
+
+    return (
+      <div>
+        <h2>Star Wars Movies</h2>
+        {movies}
+      </div>
+    )
   }
 }
 
