0185f947ce35373b10c2c978fd47e545a35850b2
Henk Bakker <spike1292@gmail.com>
2019-01-19 19:29:39 +0100

    improve preview

--- frontend/pages/preview.tsx
+++ frontend/pages/preview.tsx
@@ -1,31 +1,36 @@
 import fetch from 'isomorphic-unfetch';
 import Error from 'next/error';
+import { WithRouterProps } from 'next/router';
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import { Config } from '../config';
 import withHeaderMenu, { IMenuProps } from '../hoc/withHeaderMenu';
+import { IWPErrorResponse } from '../types/error';
 import { IWpPost } from '../types/post';
 
-interface IOwnProps {
+interface IPageQuery {
+  id: string;
+  wpnonce: string;
+}
+
+interface IPreviewProps extends IMenuProps, WithRouterProps<IPageQuery> {
   url: {
     query: {
-      id: number;
-      wpnonce: string;
+      [key: string]: string;
     };
   };
 }
 
-interface IState {
-  post: IWpPost | null;
+interface IPreviewState {
+  post?: IWpPost;
+  error?: IWPErrorResponse;
 }
 
-type IProps = IOwnProps & IMenuProps;
-
-class Preview extends Component<IProps, IState> {
-  constructor(props: IProps) {
+class Preview extends Component<IPreviewProps, IPreviewState> {
+  constructor(props: IPreviewProps) {
     super(props);
     this.state = {
-      post: null
+      post: undefined
     };
   }
 
@@ -34,32 +39,37 @@ class Preview extends Component<IProps, IState> {
     fetch(
       `${
         Config.apiUrl
-      }/wp-json/postlight/v1/post/preview?id=${id}&_wpnonce=${wpnonce}`,
+      }/wp-json/headless/v1/post/preview?id=${id}&_wpnonce=${wpnonce}`,
       { credentials: 'include' } // required for cookie nonce auth
     )
       .then(res => res.json())
       .then(res => {
-        this.setState({
-          post: res
-        });
+        if (res.code || res.code === 'rest_cookie_invalid_nonce') {
+          this.setState({
+            error: res
+          });
+        } else {
+          this.setState({
+            post: res
+          });
+        }
       });
   }
 
   public render() {
-    if (
-      this.state.post &&
-      this.state.post.code &&
-      this.state.post.code === 'rest_cookie_invalid_nonce'
-    ) {
-      return <Error statusCode={404} />;
+    const { headerMenu } = this.props;
+    const { post, error } = this.state;
+
+    if (error) {
+      return <Error statusCode={error.data.status || 404} />;
     }
 
     return (
-      <Layout menu={this.props.headerMenu}>
-        <h1>{this.state.post ? this.state.post.title.rendered : ''}</h1>
+      <Layout menu={headerMenu}>
+        <h1>{post ? post.title.rendered : ''}</h1>
         <div
           dangerouslySetInnerHTML={{
-            __html: this.state.post ? this.state.post.content.rendered : ''
+            __html: post ? post.content.rendered : ''
           }}
         />
       </Layout>
