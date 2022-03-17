5d19da0ef20608d4e979a73f49b1cb4b4fe855ff
Henk Bakker <spike1292@gmail.com>
2022-03-17 22:40:05 +0100

    implement wp service

--- frontend/services/wordpress.service.ts
+++ frontend/services/wordpress.service.ts
@@ -5,42 +5,64 @@ import { IWpPage } from "../types/page";
 import { IWpPost } from "../types/post";
 import { JSONAPIError } from "./JSONAPIError";
 
+async function fetchAPI(url: string) {
+  const response = await fetch(`${Config.apiUrl}${url}`, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  if (!response.ok) {
+    console.error(
+      `Failed to load '${response.url}'`,
+      response.status,
+      response.statusText,
+      await response.text()
+    );
+    // convert non-2xx HTTP responses into errors:
+    throw new JSONAPIError(response.statusText, response);
+  }
+  return response.json();
+}
+
+function getSlug(slugs: string | string[]): string {
+  return encodeURIComponent(Array.isArray(slugs) ? slugs[0] : slugs);
+}
+
 class WordpressService {
   public async getPosts(): Promise<IWpPost[]> {
-    // TODO Get posts from API
-    return Promise.resolve([] as any);
+    return fetchAPI("/wp-json/wp/v2/posts?_embed");
   }
 
   public async getPages(): Promise<IWpPage[]> {
-    // TODO Get pages from API
-    return Promise.resolve([] as any);
+    return fetchAPI("/wp-json/wp/v2/pages?_embed");
   }
 
-  public async getPost(slug: string): Promise<IWpPost> {
-    // TODO Get post by slug from API
-    return Promise.resolve({} as any);
+  public async getMovies(): Promise<IWpPost[]> {
+    return fetchAPI("/wp-json/wp/v2/movies?_embed");
   }
 
-  public async getPage(slug: string): Promise<IWpPage> {
+  public async getPost<T = {}>(
+    slugs: string | string[] = ""
+  ): Promise<IWpPost<T>> {
+    const slug = getSlug(slugs);
+    return fetchAPI(`/wp-json/headless/v1/post?_embed&slug=${slug}`);
+  }
+
+  public async getMovie<T = {}>(
+    slugs: string | string[] = ""
+  ): Promise<IWpPost<T>> {
+    const slug = getSlug(slugs);
+    return fetchAPI(`/wp-json/headless/v1/movies?_embed&slug=${slug}`);
+  }
+
+  public async getPage(slugs: string | string[] = ""): Promise<IWpPage> {
+    const slug = getSlug(slugs);
     // TODO Get page by slug from API
     return Promise.resolve({} as any);
   }
 
   public async getMenu(): Promise<IWpMenu> {
-    const response = await fetch(
-      `${Config.apiUrl}/wp-json/menus/v1/menus/header-menu`,
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      }
-    );
-    if (!response.ok) {
-      // convert non-2xx HTTP responses into errors:
-      const error = new JSONAPIError(response.statusText, response);
-      return Promise.reject(error);
-    }
-    return response.json();
+    return fetchAPI("/wp-json/menus/v1/menus/header-menu");
   }
 }
 
