a66b933a769eebc0669ab94627e61b90cef1e39c
Henk Bakker <spike1292@gmail.com>
2022-03-18 14:42:25 +0100

    remove implementions

--- frontend/services/wordpress.service.ts
+++ frontend/services/wordpress.service.ts
@@ -32,63 +32,66 @@ function getSlug(slugs: string | string[]): string {
 
 class WordpressService {
   public async getPosts(): Promise<IWpPost[]> {
-    return fetchAPI("/wp-json/wp/v2/posts?_embed");
+    // TODO Get data from API
+    return Promise.resolve({} as any);
   }
 
   public async getPages(): Promise<IWpPage[]> {
-    return fetchAPI("/wp-json/wp/v2/pages?_embed");
+    // TODO Get data from API
+    return Promise.resolve({} as any);
+
   }
 
   public async getMovies(): Promise<IWpPost[]> {
-    return fetchAPI("/wp-json/wp/v2/movies?_embed");
+    // TODO Get data from API
+    return Promise.resolve({} as any);
+
   }
 
   public async getPost<T = {}>(
     slugs: string | string[] = ""
   ): Promise<IWpPost<T>> {
-    const slug = getSlug(slugs);
-    return fetchAPI(`/wp-json/headless/v1/post?_embed&slug=${slug}`);
+    // TODO Get data from API
+    return Promise.resolve({} as any);
+
   }
 
   public async getMovie<T = {}>(
     slugs: string | string[] = ""
   ): Promise<IWpPost<T>> {
-    const slug = getSlug(slugs);
-    const movies = await fetchAPI(`/wp-json/wp/v2/movies?_embed&slug=${slug}`);
-    // querying all movies for slug, result is an array
-    return movies[0];
+    // TODO Get data from API
+    return Promise.resolve({} as any);
+
   }
 
   public async getPage(slugs: string | string[] = ""): Promise<IWpPage> {
     const slug = getSlug(slugs);
-    return fetchAPI(`/wp-json/headless/v1/page?_embed&slug=${slug}`);
+    // TODO Get data from API
+    return Promise.resolve({} as any);
+
   }
 
   public async getTypeByCategory(
     type: WpCategoryTypes,
     categoryId: number
   ): Promise<IWpPost[]> {
-    return fetchAPI(`/wp-json/wp/v2/${type}?_embed&categories=${categoryId}`);
+    // TODO Get data from API
+    return Promise.resolve([] as any);
+
   }
 
   public async getCategories(
     slugs: string | string[] = ""
   ): Promise<IWpCategory[]> {
-    const slug = getSlug(slugs);
-    const slugQS = `${slug ? `slug=${slug}` : ""}`;
-    try {
-      return fetchAPI(`/wp-json/wp/v2/categories?${slugQS}`);
-    } catch (e) {
-      return [];
-    }
+    // TODO Get data from API
+    return Promise.resolve([] as any);
+
   }
 
   public async getCategory(id: number): Promise<IWpCategory | undefined> {
-    try {
-      return fetchAPI(`/wp-json/wp/v2/categories/${id}`);
-    } catch (e) {
-      return;
-    }
+    // TODO Get data from API
+    return Promise.resolve([] as any);
+
   }
 
   public async getMenu(): Promise<IWpMenu> {
